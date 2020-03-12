8e857851dad06633811d32063e77aaf513a96a90
JokedeBoer <jokedeboer@gmail.com>
2020-03-12 15:00:36 +0100

    top 20

--- code/corona.R
+++ code/corona.R
@@ -3,4 +3,13 @@
 install.packages("coronavirus")
 library(coronavirus)
 corona <- data("coronavirus")
+require(dplyr)
 
+
+# Get top confirmed cases by state
+coronavirus %>%
+  filter(type == "confirmed") %>%
+  group_by(Country.Region) %>%
+  summarise(total = sum(cases)) %>%
+  arrange(-total) %>%
+  head(20)
